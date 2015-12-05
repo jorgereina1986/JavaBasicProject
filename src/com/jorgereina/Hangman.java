c2b8f4af0819bb3f2f77acd429b648824195b08a
jorgereina1986 <jorgereina1986@gmail.com>
2015-12-05 14:47:18 -0500

    started hangman

---
+++ src/com/jorgereina/Hangman.java
@@ -0,0 +1,29 @@
+package com.jorgereina;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Jorge Reina on 12/5/15.
+ */
+public class Hangman
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        List<String> secretWords = new ArrayList<String>();
+        secretWords.add("access");
+        secretWords.add("car");
+        secretWords.add("hello");
+
+        String welcome = "Welcome to hangman!";
+
+        
+
+
+
+    }
+}
